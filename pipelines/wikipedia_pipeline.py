28c221d1aefff32106289ec4cf3dace5550096bb
Rafa <jr.vera.ma@gmail.com>
2024-02-16 21:54:54 +0100

    Transforming DATA | No image

---
+++ pipelines/wikipedia_pipeline.py
@@ -0,0 +1,82 @@
+import json
+
+NO_IMAGE = 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/No-image-available.png/480px-No-image-available.png'
+
+
+def get_wikipedia_page(url):
+    import requests
+
+    print("Getting wikipedia page...", url)
+
+    try:
+        response = requests.get(url, timeout=10)
+        response.raise_for_status()  # check if the request is succesfull
+
+        return response.text
+    except requests.RequestException as e:
+        print(f"An error ocurred: {e}")
+
+
+def get_wikipedia_data(html):
+    from bs4 import BeautifulSoup
+
+    soup = BeautifulSoup(html, 'html.parser')
+    table = soup.find_all("table", {"class": "wikitable sortable"})[0]
+
+    table_rows = table.find_all('tr')
+
+    return table_rows
+
+
+def clean_text(text):
+    text = str(text).strip()
+    text = text.replace('&nbsp', '')
+    if text.find(' ♦'):
+        text = text.split(' ♦')[0]
+    if text.find('[') != -1:
+        text = text.split('[')[0]
+    if text.find('(formerly)') != -1:
+        text = text.split('(formerly)')
+    if text == '\n':
+        return ""
+
+    return text.replace('\n', '')
+
+
+def extract_wikipedia_data(**kwargs):
+    url = kwargs['url']
+    html = get_wikipedia_page(url)
+    rows = get_wikipedia_data(html)
+
+    data = []
+
+    for i in range(1, len(rows)):
+        tds = rows[i].find_all('td')
+        values = {
+            'rank': i,
+            'stadium': clean_text(tds[0].text),
+            'capacity': clean_text(tds[1].text).replace(',', '.'),
+            'region': clean_text(tds[2].text),
+            'country': clean_text(tds[3].text),
+            'city': clean_text(tds[4].text),
+            'images': 'https://' + tds[5].find('img').get('src').split("//")[1] if tds[5].find('img') else "NO IMAGE",
+            'home_team': clean_text(tds[6].text),
+        }
+        data.append(values)
+
+    json_rows = json.dumps(data)
+    kwargs['ti'].xcom_push(key='rows', value=json_rows)
+    return "OK"
+
+
+# Transform layer
+
+def transform_wikipedia_data(**kwargs):
+    import pandas as pd
+    data = kwargs['ti'].xcom_pull(key='rows', task_ids='extract_data_from_wikipedia')
+
+    data = json.loads(data)
+
+    stadiums_df = pd.DataFrame(data)
+
+    stadiums_df['images'] = stadiums_df['images'].apply(lambda x: x if x not in ['NO IMAGE', '', None] else NO_IMAGE)
